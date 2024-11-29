0eec661e2657481b167ad7c4dd050aac3cc3043c
Daniel Zazzali <dazazzali@gmail.com>
2024-11-29 12:25:01 -0300

    API done!

--- app.py
+++ app.py
@@ -15,55 +15,7 @@ app.register_blueprint(wifi_bp)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-def test_api():
-    with app.test_client() as client:
-
-        #############################################################################
-        ###################### Test the mode API  ###################################
-        #############################################################################
-
-        # Test the device mode API
-        device_mode = client.get('/device_mode')
-        print(device_mode.get_json())
-
-        device_mode = client.post('/device_mode', json={'mode': 'NOTAPNORSTA'})
-        print(device_mode.get_json())
-
-        #device_mode = client.post('/device_mode', json={'mode': 'AP'})
-        #print(device_mode.get_json())
-
-        #############################################################################
-        ###################### Test the ethernet API  ###############################
-        #############################################################################
-
-        ethernet_ip_and_mask = client.get('/ethernet_ip_and_mask')
-        print(ethernet_ip_and_mask.get_json())
-
-        ethernet_ip_and_mask = client.post('/set_ethernet_ip_and_mask', json={'ip': '172.16.23.24', 'mask': '24'})
-        print(ethernet_ip_and_mask.get_json())
-
-        ethernet_ip_and_mask = client.post('/set_ethernet_ip_and_mask', json={'ip': '172.16.23.23', 'mask': '24'})
-        print(ethernet_ip_and_mask.get_json())
-
-        #############################################################################
-        ###################### Test the wi-fi API  ##################################
-        #############################################################################
-
-        wifi_ip_and_mask = client.get('/wifi_ip_and_mask')
-        print(wifi_ip_and_mask.get_json())
-
-        remembered_wifi_connections = client.get('/remembered_wifi_connections')
-        print(remembered_wifi_connections.get_json())
-
-        scan_wifi_networks = client.get('/scan_wifi_networks')
-        print(scan_wifi_networks.get_json())
-
-        active_wifi_network = client.get('/active_wifi_network')
-        print(active_wifi_network.get_json())
-
+    return "Hello World"
 
 if __name__ == '__main__':
-    test_api()
-    #app.run(port=port)
+    app.run(port=port)
